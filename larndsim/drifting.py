7458a8c0dc9a0c4ae08950d46716afef043b428e
Youssef Nashed <youssef.shady@gmail.com>
2021-08-09 15:36:58 -0500

    prelimenary drift_with_diff

--- larndsim/drifting.py
+++ larndsim/drifting.py
@@ -9,10 +9,12 @@ from . import consts
 from .consts import tpc_borders
 
 import logging
+
 logging.basicConfig()
 logger = logging.getLogger("drifting")
 logger.setLevel(logging.WARNING)
 logger.info("DRIFTING MODULE PARAMETERS")
+
 
 @cuda.jit
 def drift(tracks):
@@ -38,19 +40,20 @@ def drift(tracks):
         pixel_plane = -1
 
         for ip, plane in enumerate(tpc_borders):
-            if plane[0][0]-2e-2 <= track["x"] <= plane[0][1]+2e-2 and \
-               plane[1][0]-2e-2 <= track["y"] <= plane[1][1]+2e-2 and \
-               min(plane[2][1]-2e-2,plane[2][0]-2e-2) <= track["z"] <= max(plane[2][1]+2e-2,plane[2][0]+2e-2):
+            if plane[0][0] - 2e-2 <= track["x"] <= plane[0][1] + 2e-2 and \
+                    plane[1][0] - 2e-2 <= track["y"] <= plane[1][1] + 2e-2 and \
+                    min(plane[2][1] - 2e-2, plane[2][0] - 2e-2) <= track["z"] <= max(plane[2][1] + 2e-2,
+                                                                                     plane[2][0] + 2e-2):
                 pixel_plane = ip
-                break 
+                break
 
         track["pixel_plane"] = pixel_plane
 
         if pixel_plane >= 0:
             z_anode = tpc_borders[pixel_plane][2][0]
             drift_distance = abs(track["z"] - z_anode) - 0.5
-            drift_start = abs(min(track["z_start"],track["z_end"]) - z_anode) - 0.5
-            drift_end = abs(max(track["z_start"],track["z_end"]) - z_anode) - 0.5
+            drift_start = abs(min(track["z_start"], track["z_end"]) - z_anode) - 0.5
+            drift_end = abs(max(track["z_start"], track["z_end"]) - z_anode) - 0.5
             drift_time = drift_distance / consts.vdrift
             lifetime_red = exp(-drift_time / consts.lifetime)
 
@@ -63,3 +66,35 @@ def drift(tracks):
             track["t_end"] += max(drift_start, drift_end) / consts.vdrift
         else:
             print(track['z'])
+
+
+try:
+    import eagerpy as ep
+
+
+    def drift_with_diff(tracks, fields):
+        tracks_ep = ep.astensor(tracks)
+        # TODO: Figure out what to do with pixel plane and tpc borders
+        pixel_plane = 0
+
+        z_anode = tpc_borders[pixel_plane][2][0]
+        drift_distance = ep.abs(tracks_ep[:, fields.index("z")] - z_anode) - 0.5
+        drift_start = ep.abs(ep.minimum(tracks_ep[:, fields.index("z_start")],
+                                        tracks_ep[:, fields.index("z_end")]) - z_anode) - 0.5
+        drift_end = ep.abs(ep.maximum(tracks_ep[:, fields.index("z_start")],
+                                      tracks_ep[:, fields.index("z_end")]) - z_anode) - 0.5
+        drift_time = drift_distance / consts.vdrift
+        lifetime_red = ep.exp(-drift_time / consts.lifetime)
+
+        tracks[:, fields.index("n_electrons")] = (tracks_ep[:, fields.index("n_electrons")] * lifetime_red).raw
+
+        tracks[:, fields.index("long_diff")] = ep.sqrt((drift_time + 0.5 / consts.vdrift) * 2 * consts.long_diff).raw
+        tracks[:, fields.index("tran_diff")] = ep.sqrt((drift_time + 0.5 / consts.vdrift) * 2 * consts.tran_diff).raw
+        tracks[:, fields.index("t")] = (tracks_ep[:, fields.index("t")] + drift_time).raw
+        tracks[:, fields.index("t_start")] = (tracks_ep[:, fields.index("t_start")] +
+                                              ep.minimum(drift_start, drift_end) / consts.vdrift).raw
+        tracks[:, fields.index("t_end")] = (tracks_ep[:, fields.index("t_end")] +
+                                            ep.maximum(drift_start, drift_end) / consts.vdrift).raw
+
+except ImportError:
+    pass
