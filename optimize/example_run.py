0888d5d95eca0d824807e224f0a5317b8680992e
Sean Gasiorowski <sgaz@slac.stanford.edu>
2022-02-10 15:29:24 -0800

    Add argparse to example_run

--- optimize/example_run.py
+++ optimize/example_run.py
@@ -1,13 +1,27 @@
 #!/usr/bin/env python3
 
 from ParamFitter import ParamFitter
+import argparse
 
-def main():
-    param_fit = ParamFitter(['eField', 'lifetime'], lr=1e1) 
-    param_fit.make_target_sim(seed=2)
-    param_fit.load_data(filename='/sdf/group/neutrino/cyifan/muon-sim/fake_data_S1/edepsim-output.h5')
-    param_fit.fit(epochs=1)
+def main(param_list, input_file, lr, epochs, seed):
+    param_fit = ParamFitter(param_list, lr=lr) 
+    param_fit.make_target_sim(seed=seed)
+    param_fit.load_data(filename=input_file)
+    param_fit.fit(epochs=epochs)
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--params", dest="param_list", default=[], nargs="+", 
+                        help="List of parameters to optimize. See consts_ep.py")
+    parser.add_argument("--input-file", dest="input_file", default="",
+                        help="Input data file")
+    parser.add_argument("--lr", dest="lr", default=1e1, type=float,
+                        help="Learning rate -- used for all params")
+    parser.add_argument("--epochs", dest="epochs", default=100, type=int,
+                        help="Number of epochs")
+    parser.add_argument("--seed", dest="seed", default=2, type=int,
+                        help="Random seed for target construction")
+   
+    args = parser.parse_args()
+    main(args.param_list, args.input_file, args.lr, args.epochs, args.seed)
