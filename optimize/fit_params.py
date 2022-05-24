32dc0df8a71c188cccc847ae5f0e3951a940e3da
Chen <cyifan@sdf-login04.slac.stanford.edu>
2022-05-24 10:40:13 -0700

    basic scan, copy from Sean's

--- optimize/fit_params.py
+++ optimize/fit_params.py
@@ -198,3 +198,118 @@ class ParamFitter:
                         pickle.dump(self.training_history, f_history)
                     if os.path.exists(f'history_epoch{n_steps-save_freq}.pkl'):
                         os.remove(f'history_epoch{n_steps-save_freq}.pkl') 
+
+
+    def loss_scan(self, dataloader, param_range=None, n_steps=10, shuffle=False, save_freq=5, print_freq=1):
+
+        # Include initial value in training history (if haven't loaded a checkpoint)
+        for param in self.relevant_params_list:
+            if len(self.training_history[param]) == 0:
+                self.training_history[param].append(getattr(self.sim_iter, param).item())
+
+        # make a folder for the pixel target
+        if os.path.exists(f'target_{param}'):
+            shutil.rmtree(f'target_{param}', ignore_errors=True)
+        os.makedirs(f'target_{param}')
+
+        # The training loop
+        with tqdm(total=len(dataloader) * epochs) as pbar:
+            for run_no, param_val in enumerate(param_vals):
+                setattr(self.sim_iter, param, param_val/ranges[param]['nom'])
+                self.sim_iter.track_gradients([param])
+
+                # Losses for each batch -- used to compute epoch loss
+                losses_batch=[]
+                grads_batch = []
+                for i, selected_tracks_bt_torch in enumerate(dataloader):
+
+                    # Get rid of the extra dimension and padding elements for the loaded data
+                    selected_tracks_bt_torch = torch.flatten(selected_tracks_bt_torch, start_dim=0, end_dim=1)
+                    selected_tracks_bt_torch = selected_tracks_bt_torch[selected_tracks_bt_torch[:, self.track_fields.index("dx")] > 0]
+                    event_id_map, unique_eventIDs = get_id_map(selected_tracks_bt_torch, self.track_fields, self.device)
+
+                    loss_ev = []
+                    # Calculate loss per event
+                    for ev in unique_eventIDs:
+                        selected_tracks_torch = selected_tracks_bt_torch[selected_tracks_bt_torch[:, self.track_fields.index("eventID")] == ev]
+                        selected_tracks_torch = selected_tracks_torch.to(self.device)
+
+                        if shuffle:
+                            target, pix_target, ticks_list_targ = all_sim(self.sim_target, selected_tracks_torch, self.track_fields,
+                                                                          event_id_map, unique_eventIDs,
+                                                                          return_unique_pix=True)
+                            embed_target = embed_adc_list(self.sim_target, target, pix_target, ticks_list_targ)
+                        else:
+                            # Simulate target and store them
+                            if run_no == 0:
+
+                                target, pix_target, ticks_list_targ = all_sim(self.sim_target, selected_tracks_torch, self.track_fields,
+                                                                              event_id_map, unique_eventIDs,
+                                                                              return_unique_pix=True)
+                                embed_target = embed_adc_list(self.sim_target, target, pix_target, ticks_list_targ)
+
+                                torch.save(embed_target, f'target_{param}/batch' + str(i) + '_ev' + str(int(ev))+ '_target.pt')
+
+                            else:
+                                embed_target = torch.load(f'target_{param}/batch' + str(i) + '_ev' + str(int(ev))+ '_target.pt')
+
+                        # Undo normalization (sim -> sim_physics)
+                        for param in self.relevant_params_list:
+                            setattr(self.sim_physics, param, getattr(self.sim_iter, param)*ranges[param]['nom'])
+                            if run_no % print_freq == 0:
+                                print(param, getattr(self.sim_physics, param))
+
+                        # Simulate and get output
+                        output, pix_out, ticks_list_out = all_sim(self.sim_physics, selected_tracks_torch, self.track_fields,
+                                                  event_id_map, unique_eventIDs,
+                                                  return_unique_pix=True)
+
+                        # Embed both output and target into "full" image space
+                        embed_output = embed_adc_list(self.sim_physics, output, pix_out, ticks_list_out)
+
+                        # Calc loss between simulated and target + backprop
+                        loss = self.loss_fn(self.sim_physics, embed_output, embed_target)
+
+                        # To be investigated -- sometimes we get nans. Avoid doing a step if so
+                        if not loss.isnan():
+                            loss_ev.append(loss)
+
+                    # Backpropagte the parameter(s) per batch
+                    if len(loss_ev) > 0:
+                        loss_ev_mean = torch.mean(torch.stack(loss_ev))
+                        loss_ev_mean.backward()
+                        losses_batch.append(loss_ev_mean.item())
+                        grads_batch.append(getattr(self.sim_iter, param).grad.item())
+
+                    pbar.update(1)
+
+                scan_losses.append(np.mean(losses_batch))
+                scan_grads.append(np.mean(np.nan_to_num(grads_batch)))
+
+            if run_no % save_freq == 0:
+                recording = {'param' : param,
+                             'param_vals': param_vals,
+                             'norm_factor' : ranges[param]['nom'],
+                             'target_val' : getattr(self.sim_target, param),
+                             'losses' : scan_losses,
+                             'grads' : scan_grads }
+
+                outname = f"loss_scan_{param}_{param_vals[0]:.02f}_{param_vals[-1]:.02f}_{run_no}"
+                with open(outname+".pkl", "wb") as f:
+                    pickle.dump(recording, f)
+                if os.path.exists(f'loss_scan_{param}_{param_vals[0]:.02f}_{param_vals[-1]:.02f}_{run_no-save_freq}.pkl'):
+                    os.remove(f'loss_scan_{param}_{param_vals[0]:.02f}_{param_vals[-1]:.02f}_{run_no-save_freq}.pkl')
+
+        recording = {'param' : param,
+                     'param_vals': param_vals,
+                     'norm_factor' : ranges[param]['nom'],
+                     'target_val' : getattr(self.sim_target, param),
+                     'losses' : scan_losses,
+                     'grads' : scan_grads }
+
+        outname = f"loss_scan_{param}_{param_vals[0]:.02f}_{param_vals[-1]:.02f}"
+        with open(outname+".pkl", "wb") as f:
+            pickle.dump(recording, f)
+
+        return recording, outname
+
---
+++ optimize/loss_landscape.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python3
+
+import argparse
+import sys, os
+import traceback
+from torch.utils.data import DataLoader
+import matplotlib.pyplot as plt
+import pickle
+
+from .fit_params import ParamFitter
+from .dataio import TracksDataset
+
+def main(config):
+    dataset = TracksDataset(filename=config.input_file, ntrack=config.data_sz)
+    tracks_dataloader = DataLoader(dataset,
+                                  shuffle=config.data_shuffle, 
+                                  batch_size=config.batch_sz,
+                                  pin_memory=True, num_workers=config.num_workers)
+    param_fit = ParamFitter(config.param_list, dataset.get_track_fields(),
+                            track_chunk=config.track_chunk, pixel_chunk=config.pixel_chunk,
+                            detector_props=config.detector_props, pixel_layouts=config.pixel_layouts,
+                            load_checkpoint=config.load_checkpoint, lr=config.lr, readout_noise=(not config.no_noise))
+    param_fit.make_target_sim(seed=config.seed)
+    landscape, fname = param_fit.loss_scan(tracks_dataloader, param_range=config.param_range, n_steps=config.n_steps, shuffle=config.data_shuffle)
+
+    if config.plot:
+        plt.plot(landscape['param_vals'], landscape['losses'])
+        y_range = max(landscape['losses']) - min(landscape['losses'])
+        x_range = max(landscape['param_vals']) - min(landscape['param_vals'])
+        lr=0.02*x_range/(max(landscape['grads']))
+        for i in range(len(landscape['param_vals'])):
+            plt.arrow(landscape['param_vals'][i], landscape['losses'][i], -lr*landscape['grads'][i], 0, 
+                      width=0.01*y_range, head_width=0.05*y_range, 
+                      head_length=0.01*x_range)
+        plt.xlabel(landscape['param'])
+        plt.ylabel('Loss')
+        plt.tight_layout()
+        plt.savefig(fname+".pdf")
+        plt.show()
+
+    return 0, 'Fitting successful'
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--params", dest="param_list", default=[], nargs="+", required=True,
+                        help="List of parameters to optimize. See consts_ep.py")
+    parser.add_argument("--input_file", dest="input_file",
+                        default="/sdf/group/neutrino/cyifan/muon-sim/fake_data_S1/edepsim-output.h5",
+                        help="Input data file")
+    parser.add_argument("--detector_props", dest="detector_props",
+                        default="larndsim/detector_properties/module0.yaml",
+                        help="Path to detector properties YAML file")
+    parser.add_argument("--pixel_layouts", dest="pixel_layouts",
+                        default="larndsim/pixel_layouts/multi_tile_layout-2.2.16.yaml",
+                        help="Path to pixel layouts YAML file")
+    parser.add_argument("--load_checkpoint", dest="load_checkpoint", type=str, default=None,
+                        help="Path to checkpoint Pickle (pkl) file")
+    parser.add_argument("--track_chunk", dest="track_chunk", default=1, type=int,
+                        help="Track chunk size used in simulation.")
+    parser.add_argument("--pixel_chunk", dest="pixel_chunk", default=1, type=int,
+                        help="Pixel chunk size used in simulation.")
+    parser.add_argument('--num_workers', type=int, default=4,
+                        help='The number of worker threads to use for the dataloader.')
+    parser.add_argument("--lr", dest="lr", default=1e1, type=float,
+                        help="Learning rate -- used for all params")
+    parser.add_argument("--batch_sz", dest="batch_sz", default=2, type=int,
+                        help="Batch size for fitting (tracks).")
+    parser.add_argument("--epochs", dest="epochs", default=100, type=int,
+                        help="Number of epochs")
+    parser.add_argument("--seed", dest="seed", default=2, type=int,
+                        help="Random seed for target construction")
+    parser.add_argument("--data_sz", dest="data_sz", default=5, type=int,
+                        help="data size for fitting (number of tracks)")
+    parser.add_argument("--param_range", dest="param_range", default=None, nargs="+", type=float,
+                        help="Param range for loss landscape")
+    parser.add_argument("--n_steps", dest="n_steps", default=10, type=int,
+                        help="Number of steps for loss landscape")
+    parser.add_argument("--plot", dest="plot", default=False, action="store_true",
+                        help="Makes landscape plot with arrows pointing in -grad direction")
+    parser.add_argument("--no-noise", dest="no_noise", default=False, action="store_true",
+                        help="Run without readout noise")
+    parser.add_argument("--data_shuffle", dest="data_shuffle", default=False, action="store_true",
+                        help="Flag of data shuffling")
+                        
+    try:
+        args = parser.parse_args()
+        retval, status_message = main(args)
+    except Exception as e:
+        print(traceback.format_exc(), file=sys.stderr)
+        retval = 1
+        status_message = 'Error: Fitting failed.'
+
+    print(status_message)
+    exit(retval)
