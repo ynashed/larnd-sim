8d8ca3195e0eafe10987c74df0e84b4bc58b2763
Sean Gasiorowski <sgaz@slac.stanford.edu>
2022-10-25 15:47:21 -0700

    Add simple plotting script/update example submission

--- optimize/scripts/example_submit.sh
+++ optimize/scripts/example_submit.sh
@@ -5,30 +5,26 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --gpus-per-node=a100:1
 #SBATCH --cpus-per-task=1
-#SBATCH --time=07:00:00
+#SBATCH --time=08:00:00
 #SBATCH --array=1,2,3,4,5
 
 INPUT_FILE=/sdf/group/neutrino/cyifan/muon-sim/fake_data_S1/edepsim-output.h5
-PARAM=tran_diff
+SIF_FILE=/sdf/group/neutrino/images/larndsim_latest.sif
 seed=$SLURM_ARRAY_TASK_ID
+PARAM=Ab
 
-export CONDA_PREFIX=/sdf/group/magis/sgaz/sw/conda/
-export PATH=${CONDA_PREFIX}/bin/:$PATH
-source ${CONDA_PREFIX}/etc/profile.d/conda.sh
-conda env list
-conda activate ml
-
-python3 -m optimize.example_run \
-  --params ${PARAM} \
-  --input_file ${INPUT_FILE} \
-  --data_sz -1 \
-  --batch_sz 5 \
-  --no-noise \
-  --num_workers 2 \
-  --track_zlen_sel 0 \
-  --out_label adam_seed${seed}_${PARAM}_test_SDTW_lr1e-2_5trk_alldset_dEdx_fix \
-  --iterations 100 \
-  --lr 1e-2 \
-  --seed ${seed} \
-  --optimizer_fn Adam \
-  --loss_fn SDTW
+singularity exec -B /sdf --nv ${SIF_FILE} \
+  python3 -m optimize.example_run \
+    --params ${PARAM} \
+    --input_file ${INPUT_FILE} \
+    --data_sz 5 \
+    --batch_sz 5 \
+    --no-noise \
+    --num_workers 2 \
+    --track_zlen_sel 0 \
+    --out_label seed${seed}_${PARAM}_adam_SDTW_lr1e-2_5trk_test \
+    --iterations 100 \
+    --lr 1e-2 \
+    --seed ${seed} \
+    --optimizer_fn Adam \
+    --loss_fn SDTW
