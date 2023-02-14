9bd28ff5740a737e8bee5683195a43644c5ab802
Pierre Granger <granger@apc.in2p3.fr>
2023-02-14 07:27:37 -0800

    Adding the pixel skipping as an option + saving the config in the output

--- optimize/example_run.py
+++ optimize/example_run.py
@@ -68,7 +68,8 @@ track_len_sel=config.track_len_sel, max_abs_costheta_sel=config.max_abs_costheta
                             fit_diffs=config.fit_diffs, optimizer_fn=config.optimizer_fn,
                             lr_scheduler=config.lr_scheduler, lr_kw=config.lr_kw,
                             no_adc=config.no_adc, loss_fn=config.loss_fn, shift_no_fit=config.shift_no_fit,
-                            link_vdrift_eField=config.link_vdrift_eField)
+                            link_vdrift_eField=config.link_vdrift_eField, batch_memory=config.batch_memory, skip_pixels=config.skip_pixels,
+                            config = config)
     param_fit.make_target_sim(seed=config.seed, fixed_range=config.fixed_range)
     param_fit.fit(tracks_dataloader, epochs=config.epochs, iterations=iterations, shuffle=config.data_shuffle, save_freq=config.save_freq)
 
@@ -165,6 +166,8 @@ if __name__ == '__main__':
                         help="Add a track to specifically consider for the analysis with the format EVENT_ID TRACK_ID. Can be used multiple times.")
     parser.add_argument("--batch_memory", dest="batch_memory", type=int, default=None,
                         help="Optimize the pixel chunk size to reach the specified GPU memory per batch, in MiB")
+    parser.add_argument("--skip_pixels", dest="skip_pixels", default=False, action="store_true",
+                        help="Iterating only over the pixels of each track (no cartesian product of all pixels x all tracks)")
 
 
     try:
